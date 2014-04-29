2377549a6223f7e8e027aede9a0666cd7570d6df
Eric Tschetter <cheddar@tidepool.org>
2014-04-29 09:41:42 -0500

    Make diasend fetch able to handle an interval as well as daysAgo

--- bin/diasend/fetch.js
+++ bin/diasend/fetch.js
@@ -33,6 +33,10 @@ argv = argv
              describe: 'Diasend password (or environment $DIASEND_PASSWORD)',
              default: process.env.DIASEND_PASSWORD
            })
+  .options('i', {
+             alias: 'interval',
+             describe: 'Interval to fetch, overrides daysAgo'
+           })
   .options('d', {
              alias: 'days',
              describe: 'Number of recent days to fetch',
@@ -47,7 +51,11 @@ if (!opts.name || !opts.username || !opts.password) {
   process.exit(1);
 }
 
-console.log('Downloading %s\'s Animas xls to include %s days of data.', opts.username, opts.days);
+if (opts.interval != null) {
+  console.log('Downloading %s\'s Animas xls for dates[%s]', opts.username, opts.interval);
+} else {
+  console.log('Downloading %s\'s Animas xls to include %s days of data.', opts.username, opts.days);
+}
 console.log('Saving xls in %s', opts.name);
 var out = fs.createWriteStream(opts.name);
 ingestion.diasend.fetch(opts, function (err, stream) {
