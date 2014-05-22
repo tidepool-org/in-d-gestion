efc0526f1bc5ab0f7f2c0172791a767ae6de67a7
Matthias Granberry <matthias.granberry@gmail.com>
2014-05-22 15:35:11 -0500

    Fix some quirks around extended and dual boluses, format numbers as done on pump, tests for extended boluses

--- test/tconnect/testParse.js
+++ test/tconnect/testParse.js
@@ -32,12 +32,11 @@ function testParser(dir) {
         var expectation = JSON.parse(fs.readFileSync(dir + '/output.json'));
         // Remove the _id field as it gets tedious to change as code changes happen
         var actualsNoId = e.map(function (element) { return _.omit(element, 'id'); });
-
         expect(actualsNoId).deep.equals(expectation);
 
         // Assert that we have a number of uniques ids equal to the number of elements
         // This is to make the removal of ids from our verification check a bit more safe
-        expect(expectation).length(e.length);
+        expect(actualsNoId.length === e.length);
 
         done();
       },
