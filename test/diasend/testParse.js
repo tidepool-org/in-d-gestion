0b5baa3e19b50c6624478c71db18a4c86913ad66
Eric Tschetter <cheddar@tidepool.org>
2014-06-11 09:41:00 -0500

    Changes to generate times in UTC

--- test/diasend/testParse.js
+++ test/diasend/testParse.js
@@ -25,7 +25,7 @@ var indigestion = require('../../lib');
 
 function testParser(dir) {
   it('should parse as expected', function (done) {
-    indigestion.diasend.parse(fs.createReadStream(dir + '/input.xls'), { timezone: 'Pacific/Honolulu' })
+    indigestion.diasend.parse(fs.createReadStream(dir + '/input.xls'), { timezone: 'America/New_York' })
       .toArray()
       .subscribe(
       function(e) {
